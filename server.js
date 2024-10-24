fa3200a4e3a0e2d44886d73c6044e9ecf26f9623
Pankaj Paliwal <pankajpaliwal2211@gmail.com>
2024-10-24 15:22:24 +0530

    changed server code1

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose");
 const { ChatOpenAI } = require("@langchain/openai"); // Import ChatOpenAI
 const cors = require("cors"); // Import CORS
 const pdfParse = require("pdf-parse");
+const timeout = require('connect-timeout');
 
 require('dotenv').config();
 
@@ -40,7 +41,7 @@ app.use(express.json());
 // app.use(express.json({ limit: '100mb' })); // Set to desired limit
 
 app.use(cors());
-app.use(express.timeout(1200000)); // Increase request timeout to 2 minutes
+app.use(timeout(1200000)); // Increase request timeout to 2 minutes
 
   
 
