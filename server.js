be9a6bc2fdb80e1bc47d0bbf9798ffcf86c20ad3
Pankaj Paliwal <158048038+pankaj-0301@users.noreply.github.com>
2024-10-25 22:59:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,12 @@ const llm = new ChatOpenAI({
 const app = express();
 const PORT = 5000;
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://lawminds-codebase.vercel.app', // Update this to your frontend URL
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}));
 
   
 
